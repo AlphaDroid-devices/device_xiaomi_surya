b197639aa39129fe866e8e65b41e6b56ea8dfb35
shoya0x00 <shoya0x00@gmail.com>
2024-07-25 18:56:48 +0600

    surya: alpha time

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -14,6 +14,33 @@ $(call inherit-product, device/xiaomi/surya/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+
+# Maintainer
+ALPHA_BUILD_TYPE := Unofficial
+ALPHA_MAINTAINER := lily985
+
+# Device config
+TARGET_ENABLE_BLUR := true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Build package
+TARGET_BUILD_PACKAGE := 3
+WITH_GMS := true
+
+# Pixel Launcher
+TARGET_LAUNCHER := 3
+
+# GAPPS
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Debugging
+TARGET_INCLUDE_MATLOG := false
+
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
